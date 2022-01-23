0fb27bb7738a198f46f6ae2d33ccfc6d2f5a7c70
Djordje99 <bozovicdjordje4@gmail.com>
2022-01-23 11:10:28 +0100

    DLL & Classes from UML

--- ModelLabsProjekat/ModelLabs/Common/Enums.cs
+++ ModelLabsProjekat/ModelLabs/Common/Enums.cs
@@ -21,6 +21,50 @@ namespace FTN.Common
 		ABC = 0xE,
 		ABCN = 0xF
 	}
+
+	public enum RegulatingControlModelKind : short
+    {
+		ActivePower = 1,
+		Admittance = 2,
+		CurrentFlow = 3,
+		Fixed = 4,
+		PowerFactor = 5,
+		ReactivePower = 6,
+		Temperature = 7,
+		TimeScheduled = 8,
+		Voltege = 9
+    }
+
+	public enum UnitSymbol : short
+    {
+		A = 1,
+		F = 2,
+		H = 3,
+		Hz = 4,
+		J = 5,
+		N = 6,
+		Pa = 7,
+		S = 8,
+		V = 9,
+		VA = 10,
+		VAh = 11,
+		VAr = 12,
+		VArh = 13,
+		W = 14,
+		Wh = 15,
+		deg = 16,
+		degC = 17,
+		g = 18,
+		h = 19,
+		m = 20,
+		m2 = 21,
+		m3 = 22,
+		min = 23,
+		none = 24,
+		ohm = 25,
+		rad = 26,
+		s = 27
+    }
 	
 	public enum TransformerFunction : short
 	{
